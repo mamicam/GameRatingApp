7fbff3ce0db0c7e1df2e7fff0797e59e2c084a4d
Michał Mamica <michal.mamica@wp.pl>
2020-10-05 12:38:41 +0200

    Added new java class 'Game'.

---
+++ src/main/java/pl/mamicam/game_rating/game/Game.java
@@ -0,0 +1,69 @@
+package pl.mamicam.game_rating.game;
+
+import java.util.Date;
+
+public class Game {
+    private long gameId;
+    private String title;
+    private String producer;
+    private Date premiereDate;
+    private String availablePlatforms;
+    private int rating;
+    private String review;
+
+    public long getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(long gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public Date getPremiereDate() {
+        return premiereDate;
+    }
+
+    public void setPremiereDate(Date premiereDate) {
+        this.premiereDate = premiereDate;
+    }
+
+    public String getAvailablePlatforms() {
+        return availablePlatforms;
+    }
+
+    public void setAvailablePlatforms(String availablePlatforms) {
+        this.availablePlatforms = availablePlatforms;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+}
